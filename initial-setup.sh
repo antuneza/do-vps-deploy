9a27c83ac3188f0bd7069189572d734c81eade42
Fernando Antunez <antuneza@gmail.com>
2014-07-24 15:45:50 +0800

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,14 +1,12 @@
 apt-get -y purge dbus libx11-6
 apt-get -y autoremove
-apt-get update
 
 echo "Asia/Hong_Kong" > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
 
+apt-get update
 apt-get -y upgrade
 apt-get -y install build-essential unzip cmake autoconf openssl
-
-mkdir /var/www
 
 fallocate -l 2G /swapfile
 chmod 600 /swapfile
@@ -19,3 +17,6 @@ echo "/swapfile   none    swap   sw   0   0" >> /etc/fstab
 echo "vm.swappiness=10" >> /etc/sysctl.conf
 echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
 
+adduser $1
+echo "$1    ALL=(ALL:ALL) ALL" > /etc/sudoers.d/$1.perm
+mkdir /var/www
